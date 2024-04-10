d846020241d7e957277e26a4b567b8d3a1b55b06
Sebastian <madxlimitzoe@gmail.com>
2024-04-10 19:40:16 +0200

    Initial commit

---
+++ TikTakToe/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TikTakToe.Models;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        _logger.LogInformation("Index action method called.");
+        return View();
+    }
+}
